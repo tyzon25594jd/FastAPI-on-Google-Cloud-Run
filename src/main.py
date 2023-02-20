8e0157afa513857a21e0baa4e617e98d90596aef
TyzonTivit <alexis.tizonb.ext@tivit.com>
2023-02-20 10:15:52 -0500

    a

--- src/main.py
+++ src/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+middleware = [
+    Middleware(
+        TrustedHostMiddleware,
+        allowed_hosts=['localhost:4200'],
+    ),
+    Middleware(CORSMiddleware, allow_origins=['*']),
+    Middleware(HTTPSRedirectMiddleware)
+]
+
+app = Starlette(middleware=middleware)
 
 
 @app.get("/")
